65d7e7d7464475f17865847a87eb5cd8bfa25392
nikozdev <nikozdev@nikozdev.net>
2023-08-23 03:46:13 -0700

    !fork!main!init!v0.0!envi!code!docs!test!tool!format
    [fork/] > added a folder for fork testing and fork files
    [fork/code/] > added a folder for source code files of the fork
    [fork/code/Main.cpp] > added a testing executable source code
    [fork/docs/] > added a folder for informational files of the fork
    [fork/docs/Readme.md] > added a brief readme file
    [fork/envi/] > added a folder for environmental files of the fork
    [fork/envi/clang-format] > added the latest nikozdev clang-format config
    [fork/envi/CMakeLists.txt] > added a basic cmake build script
    [fork/make/] > added a folder for generated files of the fork
    [fork/make/.gitignore] > added git file to ignore generated files
    [make/] > added a folder for generated files
    [make/.gitignore] > added git file to ignore generated files

---
+++ fork/code/Main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string_view>
+#include <functional>
+#include <unordered_map>
+//forward
+typedef struct GLFWwindow GLFWwindow;
+//typedef
+using tTestKey = std::string_view;
+using tTestFun = std::function<int(void)>;
+using tTestTab = const std::unordered_map<tTestKey, tTestFun>;
+using tTestRef = tTestTab::const_iterator;
+//actions
+int main(int vArgC, char **vArgV)
+{
+  if (vArgC == 3 ? (std::string_view(vArgV[1]) == "test") : 0)
+  {
+    extern tTestTab vTestTab;
+    tTestRef vTestRef = vTestTab.find(vArgV[2]);
+    if (vTestRef == vTestTab.end())
+    {
+      return EXIT_FAILURE;
+    }
+    else
+    {
+      std::cout << vTestRef->first << "=" << std::endl;
+      auto vCode = vTestRef->second();
+      std::cout << "=" << vTestRef->first << std::endl;
+      return vCode;
+    }
+  }
+  else
+  {
+    std::copy(
+      &vArgV[0],
+      &vArgV[vArgC],
+      std::ostream_iterator<char*>(std::cout, "\n")
+    );
+  }
+  return EXIT_SUCCESS;
+}//main
+tTestTab vTestTab = {
+  { "HelloWorld",
+    []()
+    {
+      std::cout << "HelloWorld" << std::endl;
+      return EXIT_SUCCESS;
+    }
+  },//HelloWorld
+};//vTestTab
---
+++ fork/docs/Readme.md
@@ -0,0 +1,6 @@
+# fork
+- maintain as a cmake-driven library
+- make this installable and usable on any mainstream os
+> macos, linux, windows
+## changelog
+- none
